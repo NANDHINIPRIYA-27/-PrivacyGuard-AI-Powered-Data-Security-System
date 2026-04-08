62d89439753f20711684e50bfbaa7c8ada6be64b
NANDHINI PRIYA <nandhinipriyasmn@gmail.com>
2026-04-08 09:33:30 +0530

    Updated Main.java with risk logic

--- Main.java
+++ Main.java
@@ -1,11 +1,60 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
-        String projectName = "PrivacyGuard AI System";
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Starting: " + projectName);
-        System.out.println("Security module loading...");
-        System.out.println("Ready to analyze permissions ✅");
+        System.out.println("=== PrivacyGuard Security Check ===");
 
+        System.out.print("Enter Application Name: ");
+        String app = sc.nextLine();
+
+        System.out.print("Enter number of permissions: ");
+        int total = sc.nextInt();
+        sc.nextLine();
+
+        int riskScore = 0;
+
+        System.out.println("\nEnter permissions:");
+
+        for (int i = 1; i <= total; i++) {
+
+            System.out.print("Permission " + i + ": ");
+            String permission = sc.nextLine().toLowerCase();
+
+            switch (permission) {
+
+                case "camera":
+                    System.out.println("Camera usage detected - High Risk");
+                    riskScore += 4;
+                    break;
+
+                case "microphone":
+                    System.out.println("Microphone usage detected - High Risk");
+                    riskScore += 4;
+                    break;
+
+                case "location":
+                    System.out.println("Location tracking detected - Medium Risk");
+                    riskScore += 2;
+                    break;
+
+                default:
+                    System.out.println(permission + " considered low risk");
+                    riskScore += 1;
+            }
+        }
+
+        System.out.println("\nFinal Risk Score: " + riskScore);
+
+        if (riskScore >= 7) {
+            System.out.println("Warning: This application may misuse your data.");
+        } else {
+            System.out.println("This application appears relatively safe.");
+        }
+
+        sc.close();
     }
 }
